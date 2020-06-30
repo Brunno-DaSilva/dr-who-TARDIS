2519d6402a0d45e9db80f4b12136003b069760f1
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-06-30 12:29:18 -0500

    App is created, constructor, and passed state to h3

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tardis: {
+        name: "Time and Relative Dimension in Space",
+        caps: false,
+      },
+    };
+  }
+
   render() {
     return (
       <div>
-        <h1> App Component</h1>
+        <h3> {this.state.tardis.name}</h3>
       </div>
     );
   }
